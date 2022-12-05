f12581c65cffe93c8a0cc9fb8ae3e1207021ee4f
SeolDonghun <ssalssi1@gmail.com>
2022-12-05 17:55:38 +0900

    init.sh modified

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ npm i -g truffle
 npm i web3 dotenv ipfs-api ipfs-http-client truffle-assertions 
 npm i -D openzeppelin-solidity
 truffle init
+
+echo ".env" >> .gitignore
+echo "node_modules" >> .gitignore
